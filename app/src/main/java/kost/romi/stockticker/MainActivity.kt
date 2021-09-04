c0104c3f77afde409b442f1f4f2ba6083338bb21
Romi <rominexus91@gmail.com>
2021-09-05 05:42:05 +0700

    second commit to main

--- app/src/main/java/kost/romi/stockticker/MainActivity.kt
+++ app/src/main/java/kost/romi/stockticker/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
+                    // test
                 }
             }
         }
